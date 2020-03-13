76d4cc9e3b1347fba001e67fc722aca3bfd97f7d
Jack Risse <j.risse14@gmail.com>
2020-03-13 15:44:43 -0400

    initial commit

---
+++ server/services/passport.js
@@ -0,0 +1,38 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const { googleClientID, googleClientSecret } = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: "http://localhost:5000/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googelId: profile.id }).then(existingUser => {
+        if (!existingUser) {
+          // already have a record of the given profile id
+          done(null, existingUser);
+        } else {
+          new User({ googelId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
