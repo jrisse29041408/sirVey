d807548d7e4d03d64ffe7b82bd8f09e25ab036a5
Jack Risse <j.risse14@gmail.com>
2020-03-14 14:34:05 -0400

    ready for production

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,8 +1,8 @@
-module.exports = {
-  googleClientID:
-    "1049307865415-6mnu42b16nnfg4ncr92s0s0j9c5t95i5.apps.googleusercontent.com",
-  googleClientSecret: "YkXnzpQajesclGojEfeL2zE2",
-  mongoURI:
-    "mongodb+srv://jackrisse:jackrisse@cluster0-krtao.mongodb.net/test?retryWrites=true&w=majority",
-  cookieKey: "adlfkjasdfiancaowie98234rjlwdjfaoe"
-};
+if (process.env.NODE_ENV === "prodution") {
+  module.exports = require("./prod");
+} else {
+  module.exports = require("./dev");
+}
+// production connect mongodb+srv://jackrisse:osNGHEcpQQH9iJuz@users-quzvy.mongodb.net/test?retryWrites=true&w=majority
+// prod clientID 314011386454-f9go8tht4q0lbce64bi3o8o888odmqul.apps.googleusercontent.com
+// prod clientSecret WZ0v36cF3fk877L36E8pQ8kt
